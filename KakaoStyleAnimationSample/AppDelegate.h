a56c69e2c14c4f4c50f05808c569bbaa1d7433e4
Lee jaeeun <dolfalf@gmail.com>
2013-11-26 09:34:55 +0900

    first commit

---
+++ KakaoStyleAnimationSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KakaoStyleAnimationSample
+//
+//  Created by Lee jaeeun on 2013/11/26.
+//  Copyright (c) 2013年 kjcode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ KakaoStyleAnimationSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  KakaoStyleAnimationSample
+//
+//  Created by Lee jaeeun on 2013/11/26.
+//  Copyright (c) 2013年 kjcode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
